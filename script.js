403f2dab473c15faa7c57e9f1c16733d9d0d0c2c
Felipefreitas020 <felipefreitasreboucas@gmail.com>
2024-10-28 16:56:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+let moveis = []
+let option = true
 
+while (option !== '3') {
+    let menu = ('  Cadastro de Imóveis : ') + deck.length + '\n\n'
+    menu += ('1.adicionar imovel:\n')
+    menu += ('2 - listar imovel\n')
+    menu += ('2 - Puxar uma carta\n')
+    menu += ( '3 - sair\n')
+    
+}
